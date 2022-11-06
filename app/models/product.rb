bb8f43fd709d134af434cf64da0d17d6c95de3b1
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-11-06 14:17:26 +0300

    done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(user_id: user.id, product_id: self.id, star_rating: star_rating, comment: comment)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+            review.print_review
+        end
+    end
+
+    def average_rating
+        self.reviews.average(:star_rating).to_f
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def reviews
+        Review.where(user_id: self.id)
+    end
+
+    def products
+        self.reviews.map do |review|
+            review.product
+        end
+    end
+
+    def favorite_product
+        self.products.order("reviews.star_rating DESC").first
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product_id: product.id).destroy_all
+    end
 end
